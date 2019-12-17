0ee51bb429ad65ff47b9600d660e5946f8688b6c
ahmedhossam96 <ahmedhossamm1996@gmail.com>
2019-12-18 00:48:30 +0200

    Update PricingController.php

--- app/Http/Controllers/PricingController.php
+++ app/Http/Controllers/PricingController.php
@@ -2,43 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use App\Area;
-use App\Order;
+use App\Servicearea;
 use App\Repairman;
+use App\Customer;
+use App\Invoice;
+use App\Order;
 use App\Service;
+
+ 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class OrdersController extends Controller
+class PricingController extends Controller
 {
     public function index()
     {
-        $areas = Area::select('id','name')->get();
-         $repairmen = Repairman::select('id','name')->get();
-       $services= Service::select('id','name')->get(); 
+    $id = Auth::id();
+    
+    $order = Order::select('id','service_id','repairman_id','customer_id','created_at')->where('repairman_id',$id)->get();
+   foreach($order as $orderr){
+    $services = Service::select('name')->where('id',$orderr->service_id)->get();
+    $repairmen = Repairman::select('name')->where('id',$orderr->repairman_id)->get();
+    $customers = Customer::select('name')->where('id',$orderr->customer_id)->get();
+    $placed_at = $orderr->created_at;
+   }
 
-       return view('order', compact('areas','services','repairmen'));
+      return view('pricing', compact('services','customers','repairmen','placed_at'));
     }
 
 
-    public function create(Request $request)
+
+    public function insert(Request $request)
+
     {
-  
         $id = Auth::id();
-      
-       // dd($request);
 
-        $order = new Order();
+        $invoice = new Invoice();
+       //Repairman::select('name')->where('id',$orderr->repairman_id)->get(); Servicearea::select('price')->where('service_id',$orderr->service_id)->get();Service::select('name')->where('id','1')->get();
+        $order = Order::select('service_id','repairman_id','id')->where('repairman_id',$id)->get();
+        foreach($order as $orderr){
+           
+        $invoice->order_id = $orderr->id;
+        $invoice->repairman_id =  1;
+        $invoice->service_id =  1;
+        $invoice->price = 30;
+        $invoice->time = $request->time;
+        $invoice->total = 30;
+        $invoice->save(); 
+        }
         
-        $order->repairman_id =  $request->repairmanid;
-        $order->service_id =  $request->serviceid;
-        $order->customer_id =  1;
-        //dd($order);
-        $order->save(); 
-        return view('orderdone');
+        return view('pricingdone');
     }
-
-
-
-
 }
+
