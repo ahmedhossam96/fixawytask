ad3991636bc5ec526f647a4ce9e9482544cef755
ahmedhossam96 <ahmedhossamm1996@gmail.com>
2019-12-18 00:46:36 +0200

    Update PricingController.php

--- app/Http/Controllers/PricingController.php
+++ app/Http/Controllers/PricingController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Area;
+use App\Order;
+use App\Repairman;
+use App\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class PricingController extends Controller
+class OrdersController extends Controller
 {
     public function index()
     {
-    return view('pricing');
+        $areas = Area::select('id','name')->get();
+         $repairmen = Repairman::select('id','name')->get();
+       $services= Service::select('id','name')->get(); 
+
+       return view('order', compact('areas','services','repairmen'));
     }
+
+
+    public function create(Request $request)
+    {
+  
+        $id = Auth::id();
+      
+       // dd($request);
+
+        $order = new Order();
+        
+        $order->repairman_id =  $request->repairmanid;
+        $order->service_id =  $request->serviceid;
+        $order->customer_id =  1;
+        //dd($order);
+        $order->save(); 
+        return view('orderdone');
+    }
+
+
+
+
 }
