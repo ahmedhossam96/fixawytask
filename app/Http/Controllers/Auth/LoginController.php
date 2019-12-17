bc91fb223000ca3836a6d569fd4ef89484b482e2
ahmedhossam96 <ahmedhossamm1996@gmail.com>
2019-12-18 00:41:15 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Customer;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
